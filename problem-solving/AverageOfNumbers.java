cf0ea612a4d89f83c3432ed7349f7f958398781f
PraveenanaiduKothapalli <pravp2410@gmail.com>
2024-11-05 19:49:57 +0530

    solved some basic codes

---
+++ problem-solving/AverageOfNumbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AverageOfNumbers {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the count of numbers : ");
+        int n=sc.nextInt();
+        int[] arr=new int[]{};
+        System.out.println("Numbers entered by user : ");
+        int sum=0;
+        for(int i=0;i<n;i++){       
+            arr[i]=sc.nextInt();
+            sum=sum+arr[i];
+        }
+        int average=sum/n;
+        System.out.println("Average of numbers entered by user : "+average);
+
+
+        
+        
+
+    }
+}
---
+++ problem-solving/SumOfSquares.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SumOfSquares {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements : ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            arr[i]=arr[i]*arr[i];
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
