e1cfd11131c9d113301396ad9707fe944e61699e
PraveenanaiduKothapalli <pravp2410@gmail.com>
2024-11-06 19:14:45 +0530

    Code formatting

--- problem-solving/SumOfFactorsOfAnumber.java
+++ problem-solving/SumOfFactorsOfAnumber.java
@@ -1,15 +1,18 @@
+
 import java.util.*;
+
 public class SumOfFactorsOfAnumber {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
                 System.out.println(i);
-                sum=sum+i;
+                sum = sum + i;
             }
-            
+
         }
         System.out.println(sum);
     }
--- problem-solving/SumOfOddNumbers.java
+++ problem-solving/SumOfOddNumbers.java
@@ -1,24 +1,26 @@
+
 import java.util.*;
+
 public class SumOfOddNumbers {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements : ");
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        int sum=0;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int sum = 0;
         System.out.println("Enter the array elements : ");
         for (int i = 0; i < n; i++) {
-            arr[i]=sc.nextInt();
-            
-           
+            arr[i] = sc.nextInt();
+
         }
-        for(int i=0;i<n;i++){
-            
-            if(arr[i]%2==1){
-                sum=sum+arr[i];
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] % 2 == 1) {
+                sum = sum + arr[i];
             }
         }
         System.out.println(sum);
-        
+
     }
 }
